b1b852821ec428d21f01644fcc5fe3acb17b5990
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-07-09 23:44:27 +0200

    first commit

---
+++ src/main/java/com/example/recettecuisine/RecetteCuisineApplication.java
@@ -0,0 +1,13 @@
+package com.example.recettecuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetteCuisineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecetteCuisineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/recettecuisine/RecetteCuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recettecuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetteCuisineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
